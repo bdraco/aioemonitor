51aba426740fcde84ae7dbf96a52c1726be4762f
J. Nick Koston <nick@koston.org>
2021-03-24 21:17:53 -1000

    Bump version: 1.0.4 → 1.0.5

--- setup.py
+++ setup.py
@@ -83,6 +83,6 @@ setup(
     url="https://github.com/bdraco/aioemonitor",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.0.4",
+    version="1.0.5",
     zip_safe=False,
 )
